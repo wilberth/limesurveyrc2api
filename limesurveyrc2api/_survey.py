cc93a192251fa2ceb2e247e341817de8d360dc62
Wilbert van Ham <w.vanham@socsci.ru.nl>
2017-09-26 10:08:47 +0200

    add export_responses

--- limesurveyrc2api/_survey.py
+++ limesurveyrc2api/_survey.py
@@ -1,4 +1,6 @@
 from collections import OrderedDict
+import base64
+import json
 from limesurveyrc2api.exceptions import LimeSurveyError
 
 
@@ -75,4 +77,45 @@ class _Survey(object):
                     raise LimeSurveyError(method, status)
         else:
             assert response_type is list
+        return response
+
+    def export_responses(self, survey_id, document_type='json'):
+        """
+        Return a list of responses from the specified survey.
+        Parameters
+        :param survey_id: ID of survey to list questions from.
+        :type survey_id: Integer
+        :param language: Language of survey to return for.
+        :type language: String
+        """
+        # string $sSessionKey, int $iSurveyID, string $sDocumentType, string $sLanguageCode = null, 
+        # string $sCompletionStatus = 'all', string $sHeadingType = 'code', string $sResponseType = 'short', 
+        # integer $iFromResponseID = null, integer $iToResponseID = null, array $aFields = null):
+        method = "export_responses"
+        params = OrderedDict([
+            ("sSessionKey", self.api.session_key),
+            ("iSurveyID", survey_id),
+            ("$sDocumentType", "json"),
+        ])
+        response = self.api.query(method=method, params=params)
+        response_type = type(response)
+
+        if response_type is dict and "status" in response:
+            status = response["status"]
+            error_messages = [
+                "Error: Invalid survey ID",
+                "Error: Invalid language",
+                "No Data, could not get max id.",
+                "No permission",
+                "Invalid session key"
+            ]
+            for message in error_messages:
+                if status == message:
+                    raise LimeSurveyError(method, status)
+        else:
+            response = base64.b64decode(response) # to bytes object
+            # unescape backslashed ( \u20ac ) unicode characters
+            response = response.decode('unicode_escape') # to unicode string
+            response = json.loads(response)['responses']
+            assert type(response) is list
         return response
